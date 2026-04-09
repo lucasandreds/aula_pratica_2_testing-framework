3a86ba2d2e404697d09474743265d28b502adfc8
lucasandreds <lucasandreufmg@gmail.com>
2026-04-09 20:13:45 -0300

    Commit 2: Classe TestResult

---
+++ TestResult.py
@@ -0,0 +1,40 @@
+from TestCase import MyTest
+
+class TestResult:
+
+    RUN_MSG = 'run'
+    FAILURE_MSG = 'failed'
+    ERROR_MSG = 'error'
+
+    def __init__(self, suite_name=None):
+        self.run_count = 0
+        self.failures = []
+        self.errors = []
+
+    def test_started(self):
+        self.run_count += 1
+
+    def add_failure(self, test):
+        self.failures.append(test)
+
+    def add_error(self, test):
+        self.errors.append(test)
+
+    def summary(self):
+        return f'{self.run_count} {self.RUN_MSG}, ' \
+               f'{str(len(self.failures))} {self.FAILURE_MSG}, ' \
+               f'{str(len(self.errors))} {self.ERROR_MSG}'
+               
+if __name__ == '__main__':
+    result = TestResult()
+
+    test = MyTest('test_a')
+    test.run(result)
+
+    test = MyTest('test_b')
+    test.run(result)
+
+    test = MyTest('test_c')
+    test.run(result)
+
+    print(result.summary())
